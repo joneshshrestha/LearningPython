1bec054eb19e2cdb7709add3e19805ddef919998
Jonesh Shrestha <contact@joneshshrestha.com>
2019-04-02 13:21:24 +0545

    learning

---
+++ odd1.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+import random
+import time
+
+odds = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49, 51, 53, 55, 57, 59]
+
+for i in range(5):
+    
+    this_now = datetime.today()
+    this_minute = this_now.minute
+
+    if this_minute in odds:
+        print("This minute seems a little odd.")
+    else:
+        print("Not an odd minute.")
+    
+    sleep_time = random.randint(1, 60)
+    time.sleep(sleep_time)
