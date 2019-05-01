81007054da686475d4803482f090b9fdd3a014c4
Jonesh Shrestha <contact@joneshshrestha.com>
2019-05-01 17:03:28 +0545

    learning

---
+++ mymodules/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='vsearch',
+    version='1.0',
+    description='The Head First Python Search Tools',
+    author='Jonesh',
+    author_email='joneshshrestha69@gmail.com',
+    url='www.joneshshrestha.com',
+    py_modules=['vsearch']
+    )
