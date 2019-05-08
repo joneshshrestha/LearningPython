ef4bcd9b9f4fa886a54164edbc4a4d4c5db7c2a2
Jonesh Shrestha <contact@joneshshrestha.com>
2019-05-08 19:01:24 +0545

    developing webapp

--- webapp/vsearch4web.py
+++ webapp/vsearch4web.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from vsearch import search4letters
 
 app = Flask(__name__)
@@ -7,12 +7,16 @@ app = Flask(__name__)
 def hello() -> str:
     return 'Hello world from Flask!'
 
-@app.route('/search4')
+@app.route('/search4', methods=['POST'])
 def do_search() -> str:
-    return str(search4letters('life, the universe, and everything', 'eiru,!'))
+    phrase = request.form['phrase']
+    letters = request.form['letters']
+    title = 'Here are your results:'
+    results = str(search4letters(phrase, letters))
+    return render_template('results.html', the_title = title, the_results = results, the_letters = letters, the_phrase = phrase)
 
 @app.route('/entry')
 def entry_page() -> 'html':
     return render_template('entry.html', the_title = 'Welcome to search4letters on the web')
 
-app.run()
+app.run(debug=True)
