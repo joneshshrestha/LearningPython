4f5ffda95ff8cee24139937755fcac8f247bd999
Jonesh Shrestha <contact@joneshshrestha.com>
2019-05-08 17:49:06 +0545

    developing webapp

--- webapp/vsearch4web.py
+++ webapp/vsearch4web.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from vsearch import search4letters
 
 app = Flask(__name__)
@@ -10,5 +10,9 @@ def hello() -> str:
 @app.route('/search4')
 def do_search() -> str:
     return str(search4letters('life, the universe, and everything', 'eiru,!'))
+
+@app.route('/entry')
+def entry_page() -> 'html':
+    return render_template('entry.html', the_title = 'Welcome to search4letters on the web')
 
 app.run()
