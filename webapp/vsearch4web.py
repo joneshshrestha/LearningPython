3b618db9b205e0e9244175fbf8eeda7b4c8bd56c
Jonesh Shrestha <contact@joneshshrestha.com>
2019-07-06 16:41:56 +0545

    new route /viewlog

--- webapp/vsearch4web.py
+++ webapp/vsearch4web.py
@@ -21,4 +21,11 @@ def do_search() -> str:
 def entry_page() -> 'html':
     return render_template('entry.html', the_title = 'Welcome to search4letters on the web')
 
-app.run(debug=True)
+@app.route('/viewlog')
+def view_the_log() -> str:
+    with open('vsearch.log') as log:
+        contents = log.read()
+    return contents
+
+if __name__ == '__main__':
+    app.run(debug=True)
