d1ea4d875e981abf6419fa53ca7b6cc22f5c6545
Jonesh Shrestha <contact@joneshshrestha.com>
2019-07-06 16:33:38 +0545

    log database file added

--- webapp/vsearch4web.py
+++ webapp/vsearch4web.py
@@ -3,12 +3,17 @@ from vsearch import search4letters
 
 app = Flask(__name__)
 
+def log_request(req: 'flask_request', res: str) -> None:
+    with open('vsearch.log', 'a') as log:
+        print(req, res, file=log)
+
 @app.route('/search4', methods=['POST'])
 def do_search() -> str:
     phrase = request.form['phrase']
     letters = request.form['letters']
     title = 'Here are your results:'
     results = str(search4letters(phrase, letters))
+    log_request(request, results)
     return render_template('results.html', the_title = title, the_results = results, the_letters = letters, the_phrase = phrase)
 
 @app.route('/')
