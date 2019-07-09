d0e8860ca14c587f598213606f3de28986473454
Jonesh Shrestha <contact@joneshshrestha.com>
2019-07-09 17:38:37 +0545

    escape

--- webapp/vsearch4web.py
+++ webapp/vsearch4web.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, escape
 from vsearch import search4letters
 
 app = Flask(__name__)
@@ -25,7 +25,7 @@ def entry_page() -> 'html':
 def view_the_log() -> str:
     with open('vsearch.log') as log:
         contents = log.read()
-    return contents
+    return escape(contents)
 
 if __name__ == '__main__':
     app.run(debug=True)
